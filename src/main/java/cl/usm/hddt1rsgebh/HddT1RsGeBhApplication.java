912473cb24f7f3796a089853b5f231a4892d75da
chthonautics <>
2026-06-26 15:12:35 -0400

    initial commit part 2

---
+++ src/main/java/cl/usm/hddt1rsgebh/HddT1RsGeBhApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.hddt1rsgebh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HddT1RsGeBhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HddT1RsGeBhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/usm/hddt1rsgebh/HddT1RsGeBhApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hddt1rsgebh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HddT1RsGeBhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
